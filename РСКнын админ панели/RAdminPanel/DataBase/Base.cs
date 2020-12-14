5aa0a9fe12b5a5633d972a6041f603c3c632d130
daniyarabdulhamidov@outlook.com <daniyarabdulhamidov@outlook.com>
2020-12-14 08:17:06 +0300

    don

--- РСКнын админ панели/RAdminPanel/DataBase/Base.cs
+++ РСКнын админ панели/RAdminPanel/DataBase/Base.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Windows.Controls;
 using MySql.Data.MySqlClient;
 
 
@@ -7,21 +8,81 @@ namespace RAdminPanel.DataBase
 {
     class Base
     {
-        public MySqlConnection connection = new MySqlConnection("datasource=192.168.0.122; port=3306;Initial Catalog='rskbank';username=Maksat;password=123456;CharSet=utf8;");
+        public MySqlConnection connection = new MySqlConnection("datasource=127.0.0.1; port=3306;Initial Catalog='rskbank';username=root;password=doni2429;CharSet=utf8;");
         public delegate void DisplaySourse(DataTable db);
         public delegate void DisplaySourse2(string[] a);
-        public event DisplaySourse eventDysplay;
         public event DisplaySourse2 eventDysplay2;
+        public delegate void SendData(DataTable data);
+        public event SendData del;
         public Base()
         {
 
+        }
+
+        public void Display(string s,int count)
+        {
+            connection.Open();
+            string[] a = new string[count];
+
+            int i = 0;
+            string sql = s;
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                a[i] = reader[0].ToString();
+                i++;
+            }
+            connection.Close();
+            eventDysplay2(a);
+        }
+
+        public void SoursDataGrid(string s,ref DataGrid data)
+        {
+            connection.Open();
+            MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = s;
+            cmd.ExecuteNonQuery();
+            DataTable dta1 = new DataTable();
+            MySqlDataAdapter dataadap = new MySqlDataAdapter(cmd);
+            dataadap.Fill(dta1);
+            data.ItemsSource = dta1.DefaultView;
+            connection.Close();         
+            
+        }
+        public void SourComboBox(string s, ref ComboBox data)
+        {          
+            connection.Open();
+            MySqlCommand cmd = new MySqlCommand(s, connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            data.ItemsSource = dt.DefaultView;
+            cmd.Dispose();
+            connection.Close();
+        }
+
+        public void SoursData(string s)
+        {
+            connection.Close();
+            connection.Open();
+            MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = s;
+            cmd.ExecuteNonQuery();
+            DataTable dta1 = new DataTable();
+            MySqlDataAdapter dataadap = new MySqlDataAdapter(cmd);
+            dataadap.Fill(dta1);
+            del(dta1);
+            connection.Close();
         }
         public void RemoveData(string table, string id)
         {
             connection.Open();
             MySqlCommand cmd = connection.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "DELETE FROM" + table + "WHERE id='" + id + "';";
+            cmd.CommandText = "DELETE FROM " + table + " WHERE id= " + id + "";
             cmd.ExecuteNonQuery();
             connection.Close();
         }
@@ -43,7 +104,6 @@ namespace RAdminPanel.DataBase
             while (reader.Read())
             {
                 value = reader[0].ToString();
-
             }
             int x = Convert.ToInt32(value);
             connection.Close();
---
+++ РСКнын админ панели/RAdminPanel/UserControlFolder/Message.xaml.cs
@@ -0,0 +1,47 @@
+﻿using RAdminPanel.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RAdminPanel.UserControlFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : Window
+    {
+        public delegate void MessageDel();
+        public event MessageDel del_;
+        Base dbCon = new Base();
+        public string Id { get; set; }
+        public string TableBasa { get; set; }
+        public Message()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Id != "" && TableBasa != "")
+                dbCon.RemoveData(TableBasa, Id);
+            del_();
+            this.Close();
+        }
+
+    }
+}
